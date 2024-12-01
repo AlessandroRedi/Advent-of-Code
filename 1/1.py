dd70f455f59a6d719d90aee7e62c264b9b702394
Alessandro Redi <alessandroredi1@gmail.com>
2024-12-01 11:05:06 +0100

    finished first aoc

---
+++ 1/1.py
@@ -0,0 +1,26 @@
+if __name__ == "__main__":
+    list1 = []
+    list2 = []
+    with open("data.txt", "r") as file:
+        for line in file:
+            input1 = line.strip()
+            num1, num2 = input1.split()
+            list1.append(int(num1))
+            list2.append(int(num2))
+
+    list1.sort()
+    list2.sort()
+    diff = [abs(i - j) for i, j in zip(list1, list2)]
+    total_sum = sum(diff)
+    print(total_sum)
+
+    similarity = 0
+    num = 0
+    for i in list1:
+        for j in list2:
+            if i == j:
+                num += 1
+        similarity += i * num
+        num = 0
+
+    print(similarity)
